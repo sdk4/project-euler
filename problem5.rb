708ad26cb426850bb0d7025d422b8c8bda8c6415
Sunil <sdk4@hw.ac.uk>
2013-05-14 13:46:29 +0100

    finished problem 5

---
+++ problem5.rb
@@ -0,0 +1,16 @@
+#problem 5 - smallest multiple (1..20)
+
+## if a number is evenly-divisible by 20, this is also true for 1,2,4,5,10
+## Divisible by 15, then also by 3,5 etc
+
+## therefore, only need to check 10..20 :)
+
+def smallest_multiple
+  num = 2520
+  while true
+    return num if (10..20).all? {|i| num%i == 0}
+    num += 20
+  end
+end
+
+print smallest_multiple
